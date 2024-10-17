1babf8a5fef311e622cb15d0ae1dd787e701ce1d
karurochari <nope>
2024-10-17 20:53:18 +0000

    jsx for xml codegen

--- scripts/codegen/components.schema.ts
+++ scripts/codegen/components.schema.ts
@@ -1,6 +1,13 @@
 import { Type as t } from "@sinclair/typebox"
 
-const type_schema = t.Union([t.Literal('string'), t.Literal('color'), t.Literal('scalar-1'), t.Literal('scalar-2'), t.Literal('scalar-4')], { description: 'type' });
+const type_schema = t.Union([t.Literal('string'), t.Literal('color'), t.Literal('scalar-1'), t.Literal('scalar-2'), t.Literal('scalar-4')], { description: 'type', default: 'string' });
+
+const entries_schema = t.Record(t.String(), t.Object({
+    type: type_schema,
+    code: t.String(),
+    description: t.Optional(t.String()),
+    alias: t.Array(t.String(), { description: "alias names" })
+}))
 
 export const widget_schema = t.Object({
     ns: t.Optional(t.String()),
@@ -13,6 +20,6 @@ export const widget_schema = t.Object({
     extends: t.Union([t.Null(), t.String()], { default: null }),
     props_tail: t.Union([t.Null(), t.String()], { default: null }),
     computed_tail: t.Union([t.Null(), t.String()], { default: null }),
-    props: t.Record(t.String(), t.Tuple([type_schema, t.String({ description: "code" }), t.Array(t.String(), { description: "alias names" })])),
-    computed: t.Record(t.String(), t.Tuple([type_schema, t.String({ description: "code" }), t.Array(t.String(), { description: "alias names" })])),
+    props: entries_schema,
+    computed: entries_schema,
 }, { additionalProperties: false })
--- scripts/codegen/gen-components.standalone.tsx
+++ scripts/codegen/gen-components.standalone.tsx
@@ -5,9 +5,12 @@
 import type { Static } from '@sinclair/typebox';
 import { $ } from 'bun'
 import { Glob } from "bun";
-import { basename, parse } from "node:path"
+import { parse } from "node:path"
 import { widget_schema } from './components.schema';
 import { Value } from '@sinclair/typebox/value';
+
+import { render, JSXXML } from 'jsx-xml'
+
 
 function gen_cpp(data: Static<typeof widget_schema>) {
     let class_decl = ``;
@@ -21,13 +24,19 @@ function gen_d_ts(data: Static<typeof widget_schema>) { }
 
 function gen_xsd(data: Static<typeof widget_schema>) { }
 
-function gen_xml_editor(data: Static<typeof widget_schema>) { }
+function gen_xml_editor(data: Static<typeof widget_schema>) {
+    return <component><props></props><computed></computed></component>
+}
 
 
 await $`rm -rf ./src/components/autogen/`
-await $`rm -rf ./include/components/autogen/`
 await $`mkdir -p ./src/components/autogen/`
+
+await $`rm -rf ./include/components/autogen/`
 await $`mkdir -p ./include/components/autogen/`
+
+await $`rm -rf ./commons/schemas/components/`
+await $`mkdir -p ./commons/schemas/components/`
 
 const glob = new Glob("./schemas/components/**/*.json");
 
@@ -42,19 +51,35 @@ for await (const file of glob.scan(".")) {
         }
     }
     else {
-        const namepieces = parse(file).base.split(':');
+        const namepieces = parse(file).name.split(':');
         tmp.ns ??= namepieces[0]
         tmp.name ??= namepieces[1]
 
+        //Cpp code
         cpp_files.push(`./src/components/autogen/${tmp.ns}/${tmp.name}.cpp`)
-        const ret = gen_cpp(tmp);
-        if (tmp.use_main_header === null) {
-            await $`mkdir -p ./include/components/autogen/${tmp.ns}/`
-            await Bun.write(`./include/components/autogen/${tmp.ns}/${tmp.name}.hpp`, ret[0])
+        {
+            const ret = gen_cpp(tmp);
+            if (tmp.use_main_header === null) {
+                await $`mkdir -p ./include/components/autogen/${tmp.ns}/`
+                await Bun.write(`./include/components/autogen/${tmp.ns}/${tmp.name}.hpp`, ret[0])
+            }
+            await $`mkdir -p ./src/components/autogen/${tmp.ns}/`
+            await Bun.write(`./src/components/autogen/${tmp.ns}/${tmp.name}.cpp`, ret[1])
+            await Bun.write('./src/ui.xml-widgets.autogen.cpp', ret[2])
         }
-        await $`mkdir -p ./src/components/autogen/${tmp.ns}/`
-        await Bun.write(`./src/components/autogen/${tmp.ns}/${tmp.name}.cpp`, ret[1])
-        await Bun.write('./src/ui.xml-widgets.autogen.cpp', ret[2])
+
+        //XML Model for the Editor
+        {
+            const ret = gen_xml_editor(tmp);
+            await $`mkdir -p ./commons/schemas/components/${tmp.ns}/`
+            await Bun.write(`./commons/schemas/components/${tmp.ns}/${tmp.name}.xml`, render(ret))
+        }
+
+        //XML Schema for aid when writing code
+        //TODO:
+
+        //Typescript types
+        //TODO:
 
         console.log(`[DONE] ${parse(file).base}`)
     }
