0d7a0637a5aba24ce6471f9b1c2f957e2a8fb64b
karurochari <nope>
2024-10-18 21:41:44 +0000

    Static xml builder now has   and specialized expression handling capabilities. Almost ready to merge

--- docs/ideas/static-templates.md
+++ docs/ideas/static-templates.md
@@ -1,6 +1,7 @@
 Static templates are generic structures for components which will be statically generated from a static dataset.  
 A typical XML technology to achieve that is XSLT.  
 However, using it has two main issues:
+
 - This project is based on `pugixml`, while the only XSLT 1.0 decently licensed implementation is `libxslt` based on `libxml2`.  
   Integrating it would be very wasteful.
 - We are going to have similar primitives offered in a dynamic version, and I would like to avoid too much divergence between the two.
@@ -8,6 +9,7 @@ However, using it has two main issues:
 As a result, an XSLT-ish preprocessor will be implemented for the generation of the final XML component.
 
 ### Component file
+
 ```xml
 <vs:component template="this://TableLike.xml">
     <format>
@@ -30,6 +32,7 @@ As a result, an XSLT-ish preprocessor will be implemented for the generation of 
 ```
 
 ### Template file
+
 ```xml
 <vs:template>
     <html:table>
@@ -48,40 +51,50 @@ As a result, an XSLT-ish preprocessor will be implemented for the generation of 
 ## Supported tags
 
 ### `for-range`
+
 - `from`
 - `to`
 - `step`
 
 ### `for` & `for-prop`
+
 To iterate over elements and props of an element respectively.
+
 - `tag` if left empty its default is `$`
 - `in` must be specified
 - `filter` if set it is a quickjs formula
 - `sort-by` (only `for`) list of relevant fields, comma separated
-- `order-by` order preference for each field, comma separated. Each entry is ASC, DESC, RANDOM  followed by a comparator name, empty if default. Default order is how it is in the source xml.
+- `order-by` order preference for each field, comma separated. Each entry is ASC, DESC, RANDOM followed by a comparator name, empty if default. Default order is how it is in the source xml.
 - `limit` maximum number of entries
 - `offset` offset from start
 
 We can also have sub-children
+
 - `header`
 - `footer`
 - `empty`
-Anything outside is assumed to be in `s-item`
+  Anything outside is assumed to be in `s-item`
 
-### `for.OP.xxx` & `for-prop.OP.xxx`
+### `for.FIELD.xxx` & `for-prop.FIELD.xxx`
+
 As prop, attribute variants of `for` and `for-props`. They add attributes to the node they are defined within.
 
-### `use` & `eval`
+### `value`
+
 To introduce the value of an expression as text content of an element.
 
-### `use.OP.xxx` & `eval.xxx`
+### `value.FIELD.xxx` & `value.xxx`
+
 As prop, to introduce the value of an expression as value of a prop `xxx`.
 
 ### `element`
+
 To generate a new element whose type is determined by a tag expression.
 
 ### `when` & `case`
+
 To perform conditional cut and paste in the final tree.  
 Attributes for `case`:
+
 - `continue` default is false.
 - `match`
--- experiments/preprocessor.cpp
+++ experiments/preprocessor.cpp
@@ -5,23 +5,33 @@
 #include <optional>
 #include <stack>
 #include <string>
+#include <variant>
 #include <vector>
 #include <pugixml.hpp>
-
-#define PREFIX_STR(name,value) char name[ns_prefix_len+std::char_traits<char>::length(value)+1];memcpy((void*)value,ns_prefix,ns_prefix_len);memcpy((void*)&value[ns_prefix_len],value,std::char_traits<char>::length(value));name[ns_prefix_len+std::char_traits<char>::length(value)]=0;
 
 
 /*
 To test:  ./build/experiments/static-preprocessor ./examples/static-building/data.xml ./examples/static-building/template.xml
 */
 
+
+#define PREFIX_STR(name,value) char name[ns_prefix_len+std::char_traits<char>::length(value)+1];memcpy(name,ns_prefix,ns_prefix_len);memcpy(name+ns_prefix_len,value,std::char_traits<char>::length(value));name[ns_prefix_len+std::char_traits<char>::length(value)]=0;
+
+template<typename T>
+const T& get_or(const auto& ref, const T& defval) noexcept{
+    if(std::holds_alternative<T>(ref))return std::get<T>(ref);
+    else return defval;
+}
+
 const char* ns_prefix = "s:";
 size_t ns_prefix_len = strlen(ns_prefix);
+
+
 
 //Utility class to implement a list of symbols. Use for `for` like structures in pattern matching.
 struct symbol_map{
     private:
-        std::vector<std::map<std::string,std::string>> symbols;
+        std::vector<std::map<std::string,std::variant<int,const pugi::xml_node, const pugi::xml_attribute>>> symbols;
     public:
         symbol_map(){
             new_frame();
@@ -35,15 +45,30 @@ struct symbol_map{
             symbols.pop_back();
         };
 
-        const char* resolve(const char* name){
+        std::string resolve_str(const char* name){
             for(auto it = symbols.rbegin();it!=symbols.rend();it++){
                 auto found = it->find(name);
-                if(found!=it->end()){return found->second.c_str();}
+                if(found!=it->end()){
+                    if(std::holds_alternative<int>(found->second))return std::to_string(std::get<int>(found->second));
+                    else if(std::holds_alternative<const pugi::xml_node>(found->second))return std::string(std::get<const pugi::xml_node>(found->second).text().as_string());
+                    else if(std::holds_alternative<const pugi::xml_attribute>(found->second))return std::string(std::get<const pugi::xml_attribute>(found->second).as_string());
+                }
             }
             return nullptr;
         }
 
-        void set(const std::string& name,const std::string& value){
+        std::optional<std::variant<int,const pugi::xml_node, const pugi::xml_attribute>> resolve(std::string_view name){
+            std::cout<<"Searching "<<name<<"\n";
+            for(auto it = symbols.rbegin();it!=symbols.rend();it++){
+                auto found = it->find(std::string(name));
+                if(found!=it->end()){
+                    return found->second;
+                }
+            }
+            return {};
+        }
+
+        void set(const std::string& name, auto value){
             symbols.back().emplace(name,value);
         }
 
@@ -74,9 +99,12 @@ struct document{
     std::stack<std::pair<pugi::xml_node_iterator,pugi::xml_node_iterator>> stack_template;
     symbol_map symbols;
 
+    pugi::xml_node root_data;
+
     document(const pugi::xml_document& root_data, const pugi::xml_document& root_template){
         stack_template.push({root_template.root().begin(),root_template.root().end()});
         stack_compiled.push(compiled.root());
+        this->root_data=root_data.root();
     }
 
     pugi::xml_document& parse (){
@@ -84,11 +112,78 @@ struct document{
         return compiled;
     }
 
+    /*
+    Allowed formats (for now):
+    {node}/... .attr
+    {node}/.../
+    /.../
+    /... .attr
+    */
+    std::optional<std::variant<int,const pugi::xml_node, const pugi::xml_attribute, std::string>> resolve_expr(const char* _str, int limit = -1){
+        int str_len = strlen(_str);
+        char str[str_len+1];
+        memcpy(str,_str,str_len+1);
+
+        pugi::xml_node ref;
+        int idx = 0;
+        if(str[0]=='.' || str[0]=='+' || str[0]=='-' || (str[0]>'0' && str[0]<'9')) return atoi(_str);
+        else if(str[0]=='{'){
+            int close = 0;
+            for(;close<str_len && str[close]!='}';close++);
+            str[close]=0;
+            auto tmp = symbols.resolve(std::string_view(str+1,str+close));
+            if(!tmp.has_value())return {};
+            else if(std::holds_alternative<const pugi::xml_node>(tmp.value())){
+                ref=std::get<const pugi::xml_node>(tmp.value());
+            }
+            else if(std::holds_alternative<int>(tmp.value())){
+                return std::get<int>(tmp.value());
+            }
+            else if(std::holds_alternative<const pugi::xml_attribute>(tmp.value())){
+                return std::get<const pugi::xml_attribute>(tmp.value());
+            }
+            if(close==str_len)return ref;        //End of string was met earlier
+            idx=close+1;
+        }
+        else if(str[0]=='$'){
+
+            auto tmp =  symbols.resolve("$");
+            if(!tmp.has_value() || std::holds_alternative<const pugi::xml_node>(tmp.value())==false)return {};
+            else{
+                ref=std::get<const pugi::xml_node>(tmp.value());
+            }
+            if(str[1]==0)return ref;            //End of string was met earlier
+            idx++;
+        }
+        else if(str[0]=='/'){
+            ref=root_data;
+            idx++;
+        }
+
+
+        for(;;){
+            int close = idx;
+            for(;close<str_len && str[close]!='/' && str[close]!='~';close++);
+            str[close]=0;
+            if(idx!=close)ref = ref.child(str+idx); //Avoid the prefix /
+            if(close==str_len )return ref;            //End of string was met earlier
+            else if(str[close]=='~'){idx=close;break;}
+            else{idx=close+1;}
+        }
+        //Process the terminal attribute
+        if(str[idx]=='~'){
+            return ref.attribute(str+idx+1).as_string();
+        }
+
+        return {};
+    }
+
+    //TODO: use resolve_expr
     std::optional<std::string> eval_expr(const std::string& expr){
-        auto t =symbols.resolve(expr.c_str());
-        if(t==nullptr)return {};
+        auto t =symbols.resolve_str(expr.c_str());
         return t;
     }
+
 
     void _parse(std::optional<pugi::xml_node_iterator> stop_at){ 
         /*
@@ -102,6 +197,7 @@ struct document{
             PREFIX_STR(CASE_TAG,"case");
         */
         while(!stack_template.empty()){
+
             auto& current_template = stack_template.top();
             auto& current_compiled = stack_compiled.top();
 
@@ -111,53 +207,112 @@ struct document{
                 //Special handling of static element
                 if(strncmp(current_template.first->name(), ns_prefix, ns_prefix_len)==0){
                     if(strcmp(current_template.first->name()+ns_prefix_len,"for-range")==0){
-                        const char* tag = current_template.first->attribute("tag").as_string("$");
-                        int from = current_template.first->attribute("from").as_int(0);
-                        int to = current_template.first->attribute("to").as_int(0);
-                        int step = current_template.first->attribute("step").as_int(1);
+                        const char* tag = current_template.first->attribute("tag").as_string();
+                        int from = get_or<int>(resolve_expr(current_template.first->attribute("from").as_string("0")).value_or(0),0);
+                        int to = get_or<int>(resolve_expr(current_template.first->attribute("to").as_string("0")).value_or(0),0);
+                        int step = get_or<int>(resolve_expr(current_template.first->attribute("step").as_string("1")).value_or(1),1);
                         for(int i=from; i<to; i+=step){
                             auto frame_guard = symbols.guard();
-                            symbols.set(tag,std::to_string(i));
-                            stack_template.push({current_template.first->children().begin(),current_template.first->children().end()});
+                            if(tag!=nullptr)symbols.set(tag,i);
+                            symbols.set("$",i);
+                            stack_template.push({current_template.first->begin(),current_template.first->end()});
                             _parse(current_template.first);
                             //When exiting one too many is removed. restore it.
                             stack_compiled.push(current_compiled);
                         }
                     }
                     else if(strcmp(current_template.first->name()+ns_prefix_len,"for")==0){
-                        const char* tag = current_template.first->attribute("tag").as_string("$");
+                        const char* tag = current_template.first->attribute("tag").as_string();
                         const char* in = current_template.first->attribute("in").as_string();
                         //filter
                         //sort-by
                         //order-by
-                        int limit = current_template.first->attribute("limit").as_int(-1);
-                        int offset = current_template.first->attribute("offset").as_int(0);
+                        int limit = get_or<int>(resolve_expr(current_template.first->attribute("limit").as_string("-1")).value_or(-1),-1);
+                        int offset = get_or<int>(resolve_expr(current_template.first->attribute("offset").as_string("0")).value_or(0),0);
 
                         bool empty=false;
+                        
+                        auto expr = resolve_expr(in);
+
                         if(empty){
-                            //TODO:Test this
                             PREFIX_STR(EMPTY_TAG,"empty");
+
                             for(const auto& el: current_template.first->children(EMPTY_TAG)){
-                                stack_template.push({el.children().begin(),el.children().end()});
-                                _parse(el);
-                                stack_compiled.push(el);
+                                stack_template.push({el.begin(),el.end()});
+                                _parse(current_template.first);
+                                stack_compiled.push(current_compiled);
+                            }
+                        }
+                        else if(!expr.has_value() || !std::holds_alternative<const pugi::xml_node>(expr.value())){ //Skip numbers since they are useless here!
+                            PREFIX_STR(ERROR_TAG,"error");
+
+                            for(const auto& el: current_template.first->children(ERROR_TAG)){
+                                stack_template.push({el.begin(),el.end()});
+                                _parse(current_template.first);
+                                stack_compiled.push(current_compiled);
                             }
                         }
                         else{
-                            //Header (once)
-                            //Items (iterate)
-                            //Footer (once)
+                                //Header (once)
+                                {
+                                    PREFIX_STR(HEADER_TAG,"header");
+                                    for(const auto& el: current_template.first->children(HEADER_TAG)){
+                                        stack_template.push({el.begin(),el.end()});
+                                        _parse(current_template.first);
+                                        stack_compiled.push(current_compiled);
+                                    }
+                                }
+                            
+                                //Items (iterate)
+                                PREFIX_STR(ITEM_TAG,"item");
+                                for(auto& i : std::get<const pugi::xml_node>(expr.value()).children()){
+                                    auto frame_guard = symbols.guard();
+                                    if(tag!=nullptr)symbols.set(tag,i);
+                                    symbols.set("$",i);
+                                    for(const auto& el: current_template.first->children(ITEM_TAG)){
+                                        stack_template.push({el.begin(),el.end()});
+                                        _parse(current_template.first);
+                                        stack_compiled.push(current_compiled);
+                                    }
+
+                                }
+
+                                //Footer (once)
+                                {
+                                    PREFIX_STR(FOOTER_TAG,"footer");
+                                    for(const auto& el: current_template.first->children(FOOTER_TAG)){
+                                        stack_template.push({el.begin(),el.end()});
+                                        _parse(current_template.first);
+                                        stack_compiled.push(current_compiled);
+                                    }
+                                }
                         }
                     }
                     else if(strcmp(current_template.first->name()+ns_prefix_len,"for-prop")==0){}
                     else if(strcmp(current_template.first->name()+ns_prefix_len,"element")==0){}
-                    else if(strcmp(current_template.first->name()+ns_prefix_len,"eval")==0){
-                        auto tmp = current_compiled.append_child(pugi::node_pcdata);
-                        auto val = eval_expr(current_template.first->text().as_string("$"));
-                        if(val.has_value()){
-                            tmp.set_value(val->c_str());
+                    else if(strcmp(current_template.first->name()+ns_prefix_len,"value")==0){
+                        auto symbol = resolve_expr(current_template.first->attribute("src").as_string("$"));
+                        if(!symbol.has_value()){
+                            /*Show default content*/
+                            stack_template.push({current_template.first->begin(),current_template.first->end()});
+                            _parse(current_template.first);
+                            stack_compiled.push(current_compiled);
                         }
-                        //current_compiled.append_buffer(current_template.first->text().as_string(),strlen(current_template.first->text().as_string()));
+                        else{
+                            if(std::holds_alternative<int>(symbol.value())){
+                                current_compiled.append_child(pugi::node_pcdata).set_value(std::to_string(std::get<int>(symbol.value())).c_str());
+                            }
+                            else if(std::holds_alternative<const pugi::xml_attribute>(symbol.value())) {
+                                current_compiled.append_child(pugi::node_pcdata).set_value(std::get<const pugi::xml_attribute>(symbol.value()).as_string());
+                            }
+                            else if(std::holds_alternative<std::string>(symbol.value())) {
+                                current_compiled.append_child(pugi::node_pcdata).set_value(std::get<std::string>(symbol.value()).c_str());
+                            }
+                            else if(std::holds_alternative<const pugi::xml_node>(symbol.value())) {
+                                auto tmp = std::get<const pugi::xml_node>(symbol.value());
+                                current_compiled.append_copy(tmp);
+                            }
+                        }
                     }
                     else if(strcmp(current_template.first->name()+ns_prefix_len,"use")==0){}
                     else if(strcmp(current_template.first->name()+ns_prefix_len,"when")==0){}
