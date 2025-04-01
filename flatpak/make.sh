a67704bf48ecfec8ff45693086e7953d18f24d7d
Karuro Chari <karurochori@users.noreply.github.com>
2025-04-01 13:43:12 +0100

    Fixing flatpack, but it looks like clang++-20 is not visible?

--- flatpak/make.sh
+++ flatpak/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo dist com.karurochari.vs.yml 
+flatpak run org.flatpak.Builder --force-clean --install-deps-from=flathub --repo=repo dist com.karurochari.vs.yml 
 flatpak build-bundle repo vs.flatpak com.karurochari.vs
