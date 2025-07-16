a8948faa46d14068214ff025afe0d8dd6b8cb5ea
Andy Alt <andy5995@users.noreply.github.com>
2025-07-16 11:16:29 -0500

    docker: Build with Alpine Linux (#58)
    * docker: Build with Alpine Linux
    
    For #50
    
    [skip ci]
    
    This image will build, but swift is not available
    
    [SR-4632] Swift on Alpine Linux (support musl libc)
    https://github.com/swiftlang/swift/issues/47209
    
    Image sizes:
    
    This is 1.1GB, the Debian image is currently 3.83
    
    This PR can be closed and use for future use.
    
    * docker-compose.yml: Add build option
    
    * Dockerfile: Fix 'bun: not found'
    
    * Dockerfile: Fix sudo not found (fix comment location)
    
    * Dockerfile: Install lld and make
    
    * entry-build.sh: source ~/.bashrc

--- docker/entry-build.sh
+++ docker/entry-build.sh
@@ -4,4 +4,4 @@ set -e
 $WORKSPACE/docker/entry-common.sh
 
 # Reminder: changing the double quotes to single will break things
-su -l builder -c "cd $WORKSPACE && scripts/build-default.sh"
+su builder -c "source ~/.bashrc && cd $WORKSPACE && scripts/build-default.sh"
